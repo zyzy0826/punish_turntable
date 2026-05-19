dea8394d34428a9897d8a18dc28b52680f944eff
zyzy0826 <user@zuiyu.me>
2026-05-19 23:09:34 +0800

    feat: 修改成 Minecraft 風格

--- punishments.js
+++ punishments.js
@@ -1,10 +1,10 @@
 const PUNISHMENTS = [
-  { label: "講個冷笑話", color: "#00fff5" },
-  { label: "原地開合跳 10 下", color: "#ff00a0" },
-  { label: "去隔壁小隊自我介紹", color: "#7b00ff" },
-  { label: "稱讚自己小隊的其中一個人", color: "#00ff41" },
-  { label: "\\OuO/", color: "#ff6600" },
-  { label: "做鬼臉", color: "#ffe600" },
-  { label: "喵喵喵", color: "#00a8ff" },
-  { label: "OAO", color: "#ff2d55" },
+  { label: "講個冷笑話", color: "#5D7C15" },
+  { label: "原地開合跳 10 下", color: "#CC0000" },
+  { label: "去隔壁小隊自我介紹", color: "#1B4FCC" },
+  { label: "稱讚自己小隊的其中一個人", color: "#866043" },
+  { label: "\\OuO/", color: "#FF8C00" },
+  { label: "做鬼臉", color: "#FCBA03" },
+  { label: "喵喵喵", color: "#53ECEC" },
+  { label: "OAO", color: "#17DD62" },
 ];
