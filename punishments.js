c1f3e79f595ba3be254cd8541548846d3a031162
zyzy0826 <user@zuiyu.me>
2026-05-20 00:46:01 +0800

    docs: 新增懲罰

--- punishments.js
+++ punishments.js
@@ -3,8 +3,8 @@ const PUNISHMENTS = [
   { label: "原地開合跳 10 下", color: "#CC0000" },
   { label: "去隔壁小隊自我介紹", color: "#1B4FCC" },
   { label: "稱讚自己小隊的其中一個人", color: "#866043" },
-  { label: "\\OuO/", color: "#FF8C00" },
-  { label: "做鬼臉", color: "#FCBA03" },
-  { label: "喵喵喵", color: "#53ECEC" },
-  { label: "OAO", color: "#17DD62" },
+  { label: "大喊注意然後謝謝大家的注意", color: "#FF8C00" },
+  { label: "隨便唱一句歌詞", color: "#FCBA03" },
+  { label: "去其他小隊表演才藝", color: "#53ECEC" },
+  { label: "諂媚隊輔", color: "#17DD62" },
 ];
