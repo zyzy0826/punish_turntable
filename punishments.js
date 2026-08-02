a9c8747a8b4009f9791d8b92e36f0c318d8f3915
zyzy0826 <user@zuiyu.me>
2026-08-03 01:00:19 +0800

    docs: 修改懲罰內容

--- punishments.js
+++ punishments.js
@@ -1,10 +1,10 @@
 const PUNISHMENTS = [
   { label: "講個冷笑話", color: "#5D7C15" },
-  { label: "原地開合跳 10 下", color: "#CC0000" },
+  { label: "跟別組每一個人擊掌", color: "#CC0000" },
   { label: "去隔壁小隊自我介紹", color: "#1B4FCC" },
   { label: "稱讚自己小隊的其中一個人", color: "#866043" },
   { label: "大喊注意然後謝謝大家的注意", color: "#FF8C00" },
-  { label: "隨便唱一句歌詞", color: "#FCBA03" },
-  { label: "去其他小隊表演才藝", color: "#53ECEC" },
-  { label: "諂媚隊輔", color: "#17DD62" },
+  { label: "說一個關於自己的事", color: "#FCBA03" },
+  { label: "每句話結尾都要加特定詞（喵/汪之類的）", color: "#53ECEC" },
+  { label: "分享一個月內的一張照片分享一下故事", color: "#17DD62" },
 ];
